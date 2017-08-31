6b1d88688eddc94c2569101f353b956ec9efb334
Nyakeh Rogers <nyakeh123@gmail.com>
2017-08-31 20:10:08 +0100

    Platformer tutorial : Created Idle animation & horizontal movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private Rigidbody2D _playerBody;
+    [SerializeField]
+    private float _movementSpeed = 10;
+    private bool facingRight = true;
+
+	void Start() {
+        _playerBody = GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate() {
+        float horizontal = Input.GetAxis("Horizontal");
+        HandleMovement(horizontal);
+        Flip(horizontal);
+	}
+
+    private void HandleMovement(float horizontal)
+    {
+        _playerBody.velocity = new Vector2(horizontal * _movementSpeed, _playerBody.velocity.y);
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+}
