0e905c1d3580124432ae2734487aee51851cdf29
Nyakeh Rogers <nyakeh123@gmail.com>
2017-08-31 20:22:32 +0100

    Added run animation including transitions between idle & running when the character moves

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,14 @@
 
 public class Player : MonoBehaviour {
     private Rigidbody2D _playerBody;
+    private Animator _playerAnimator;
     [SerializeField]
     private float _movementSpeed = 10;
     private bool facingRight = true;
 
 	void Start() {
         _playerBody = GetComponent<Rigidbody2D>();
+        _playerAnimator = GetComponent<Animator>();
 	}
 	
 	void FixedUpdate() {
@@ -19,6 +21,7 @@ public class Player : MonoBehaviour {
     private void HandleMovement(float horizontal)
     {
         _playerBody.velocity = new Vector2(horizontal * _movementSpeed, _playerBody.velocity.y);
+        _playerAnimator.SetFloat("speed", Mathf.Abs(horizontal));
     }
 
     private void Flip(float horizontal)
